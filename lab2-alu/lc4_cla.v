6cca509c3fe7fda60fa15e9bb78fc3d59de17bdd
Nathan J Rush <narush@big13.seas.upenn.edu>
2020-02-09 01:21:11 -0500

    add gp4

--- lab2-alu/lc4_cla.v
+++ lab2-alu/lc4_cla.v
@@ -13,6 +13,20 @@ module gp1(input wire a, b,
 endmodule
 
 /**
+ * @param a first 2-bit input
+ * @param b second 2-bit input
+ * @param g whether a and b generate a carry
+ * @param p whether a and b would propagate an incoming carry
+ */
+module gp2(input wire [1:0] gin, pin,
+           input wire cin,
+           output wire gout, pout, cout);
+   assign gout = gin[1] | (gin[0] & pin[1]);
+   assign pout = pin[0] & pin[1];
+   assign cout = gout | (pout & cin);
+endmodule
+
+/**
  * Computes aggregate generate/propagate signals over a 4-bit window.
  * @param gin incoming generate signals 
  * @param pin incoming propagate signals
@@ -25,6 +39,30 @@ module gp4(input wire [3:0] gin, pin,
            input wire cin,
            output wire gout, pout,
            output wire [2:0] cout);
+   wire [1:0] g_1_0, p_1_0, g_3_2, p_3_2;
+   wire cout_1, cout_2, cout_3;
+
+   gp2 h0(
+      .gin(gin[1:0]), 
+      .pin(pin[1:0]), 
+      .cin(cin), 
+      .gout(g_1_0), 
+      .pout(p_1_0), 
+      .cout(cout_1)
+   );
+   gp2 h1(
+      .gin(gin[3:2]), 
+      .pin(pin[3:2]), 
+      .cin(cout_1), 
+      .gout(g_3_2), 
+      .pout(p_3_2), 
+      .cout(cout_2)
+   );
+
+   assign gout = g_3_2 | (g_1_0 & p_3_2);
+   assign pout = p_1_0 & p_3_2;
+   assign cout_3 = gout | (pout & cin);
+   assign cout = {cout_1, cout_2, cout_3};
    
 endmodule
 
