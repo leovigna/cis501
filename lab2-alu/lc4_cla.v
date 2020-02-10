83edd60ae77907c9fc37397268feaf019d3c04b8
Nathan J Rush <narush@big08.seas.upenn.edu>
2020-02-10 02:40:22 -0500

    cla adder

--- lab2-alu/lc4_cla.v
+++ lab2-alu/lc4_cla.v
@@ -13,20 +13,6 @@ module gp1(input wire a, b,
 endmodule
 
 /**
- * @param a first 2-bit input
- * @param b second 2-bit input
- * @param g whether a and b generate a carry
- * @param p whether a and b would propagate an incoming carry
- */
-module gp2(input wire [1:0] gin, pin,
-           input wire cin,
-           output wire gout, pout, cout);
-   assign gout = gin[1] | (gin[0] & pin[1]);
-   assign pout = pin[0] & pin[1];
-   assign cout = gout | (pout & cin);
-endmodule
-
-/**
  * Computes aggregate generate/propagate signals over a 4-bit window.
  * @param gin incoming generate signals 
  * @param pin incoming propagate signals
@@ -42,28 +28,53 @@ module gp4(input wire [3:0] gin, pin,
    wire [1:0] g_1_0, p_1_0, g_3_2, p_3_2;
    wire cout_1, cout_2, cout_3;
 
-   gp2 h0(
-      .gin(gin[1:0]), 
-      .pin(pin[1:0]), 
-      .cin(cin), 
-      .gout(g_1_0), 
-      .pout(p_1_0), 
-      .cout(cout_1)
-   );
-   gp2 h1(
-      .gin(gin[3:2]), 
-      .pin(pin[3:2]), 
-      .cin(cout_1), 
-      .gout(g_3_2), 
-      .pout(p_3_2), 
-      .cout(cout_2)
+   assign cout[0] = gin[0] | (pin[0] & cin);
+   assign cout[1] = gin[1] | (pin[1] & cout[0]);
+   assign cout[2] = gin[2] | (pin[2] & cout[1]);
+
+   assign gout = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]);
+   assign pout = p[3] & p[2] & p[1] & p[0];
+   
+endmodule
+
+/**
+ * 4-bit Carry-Lookahead Adder
+ * @param a first input
+ * @param b second input
+ * @param cin carry in
+ * @param sum sum of a + b + carry-in
+ */
+module cla4
+  (input wire [3:0]  a, b,
+   input wire cin,
+   output wire [3:0] sum
+   output wire cout);
+
+   wire [3:0] gin, pin;
+   wire [2:0] cout_tmp;
+   wire gout, pout;
+
+   gp1 g1(.a(a[0]), .b(b[0]), .g(gin[0]), .p(pin[0]));
+   gp1 g2(.a(a[1]), .b(b[1]), .g(gin[1]), .p(pin[1]));
+   gp1 g3(.a(a[2]), .b(b[2]), .g(gin[2]), .p(pin[2]));
+   gp1 g4(.a(a[3]), .b(b[3]), .g(gin[3]), .p(pin[3]));
+
+   gp4 g5(
+      .gin(gin), 
+      .pin(pin),
+      .cin(cin),
+      .gout(gout), 
+      .pout(pout),
+      .cout(cout_tmp)
    );
 
-   assign gout = g_3_2 | (g_1_0 & p_3_2);
-   assign pout = p_1_0 & p_3_2;
-   assign cout_3 = gout | (pout & cin);
-   assign cout = {cout_1, cout_2, cout_3};
-   
+   assign sum[0] = (a[0] ^ b[0] ^ cin); // not sure about this
+   assign sum[1] = (a[1] ^ b[1]) ^ cout_tmp[0];
+   assign sum[2] = (a[2] ^ b[2]) ^ cout_tmp[1];
+   assign sum[3] = (a[3] ^ b[3]) ^ cout_tmp[2];
+
+   assign cout = gout | (pout & cin);
+
 endmodule
 
 /**
@@ -77,5 +88,39 @@ module cla16
   (input wire [15:0]  a, b,
    input wire         cin,
    output wire [15:0] sum);
+
+   wire cout_1, cout_2, cout_3, cout_4;
+
+   cla4 c1(
+      .a(a[3:0]),
+      .b(b[3:0]),
+      .cin(cin)
+      .sum(sum[3:0]),
+      .cout(cout_1)
+   );
+
+   cla4 c2(
+      .a(a[7:4]),
+      .b(b[7:4]),
+      .cin(cout_1)
+      .sum(sum[7:3]),
+      .cout(cout_2)
+   );
+
+   cla4 c3(
+      .a(a[11:8]),
+      .b(b[11:8]),
+      .cin(cout_2)
+      .sum(sum[11:8]),
+      .cout(cout_3)
+   );
+
+   cla4 c4(
+      .a(a[15:12]),
+      .b(b[15:12]),
+      .cin(cout_3)
+      .sum(sum[15:12]),
+      .cout(cout_4)
+   );
 
 endmodule
